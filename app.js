f7a8a96efd044f442b51d73f93e687c71a4ce75d
Artem Petselia <mrtimego@ukr.net>
2022-09-21 22:04:46 +0300

    profile page done

---
+++ app.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenImages = document.querySelectorAll('.hidden')
+hiddenImages.forEach((element) => observer.observe(element))
